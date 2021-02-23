1469f19cc1c7ed3b97ee09367f929ef245f334a5
Aream Luersen <aream@geomais.com.br>
2021-02-23 17:31:10 -0300

    feat: criado projeto geo com antd. initial commit :)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
       </header>
     </div>
   );
