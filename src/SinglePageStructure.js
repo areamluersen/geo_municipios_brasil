8da1498ac2e68087ed10741aa5030df8df4fe2d8
Aream <aream@geomais.com.br>
2021-03-06 12:47:13 -0300

    feat: added eslint to project

--- src/SinglePageStructure.js
+++ src/SinglePageStructure.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './SinglePageStructure.css'
+import './SinglePageStructure.css';
 
-export default function SinglePageStructure(){
-    return (
-        <div className="grid-container">
-        <header className="header">
-          <span>Mapa da obesidade no brasil</span>
-        </header>
-        <aside className="base-sidenav">
-          <div className="sidenav"></div>
-        </aside>
-        <main className="main">
-          <div id="here-is-the-map" style={{width: '100%', height: '100%'}}/>
-        </main>
-        </div>
-    )
+export default function SinglePageStructure() {
+  return (
+    <div className="grid-container">
+      <header className="header">
+        <span>Mapa da obesidade no brasil</span>
+      </header>
+      <aside className="base-sidenav">
+        <div className="sidenav" />
+      </aside>
+      <main className="main">
+        <div id="here-is-the-map" style={{ width: '100%', height: '100%' }} />
+      </main>
+    </div>
+  );
 }
