fcb96ad03e711efaee22f0d2a6b14cc2d16e314f
Aream <aream@geomais.com.br>
2021-03-06 12:19:48 -0300

    refactor: moved some styles to css file

--- src/SinglePageStructure.js
+++ src/SinglePageStructure.js
@@ -4,17 +4,11 @@ import './SinglePageStructure.css'
 export default function SinglePageStructure(){
     return (
         <div className="grid-container">
-            <header className="header">
-          <span>Simulação Discreta - Lavacar wash</span>
+        <header className="header">
+          <span>Mapa da obesidade no brasil</span>
         </header>
-        <aside className="sidenav">
-          <div style={{
-              backgroundColor: '#49505f', 
-              borderRadius: 10, 
-              margin: '0px 10px 30px', 
-              boxShadow: '5px 5px 5px rgba(0,0,0,0.5)', 
-              width: '95%', 
-              height: '98%'}}></div>
+        <aside className="base-sidenav">
+          <div className="sidenav"></div>
         </aside>
         <main className="main">
           <div id="here-is-the-map" style={{width: '100%', height: '100%'}}/>
