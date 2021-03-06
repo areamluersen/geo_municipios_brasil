0cc59f9adc2536e0830a9953d8218e9f1c6cc8a1
Aream <aream@geomais.com.br>
2021-03-06 12:12:02 -0300

    feat: base layout for singlePage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import map from './components/mapboxgl'
 import {useEffect} from 'react';
+import SinglePageStructure from './SinglePageStructure'
 
 function App() {
   useEffect(() => {
     map()
   },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <div id="here-is-the-map" style={{width: 800, height: 800}}/>
-      </header>
-    </div>
+    <SinglePageStructure />
   );
 }
 
